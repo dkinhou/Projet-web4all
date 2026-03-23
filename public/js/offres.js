ac2e35b48903c9fc6da30411a611e1979b4fbd92
Chd <lama664fache@gmail.com>
2026-03-23 11:14:00 +0100

    Page offre avec bouton coeur pour la wishlist git push git fetch origin

--- public/js/offres.js
+++ public/js/offres.js
@@ -22,3 +22,22 @@ popupOverlay.addEventListener('click', function(event){
         popupOverlay.classList.remove('active');
     }
 });
+
+//Pour gérer le bouton qui permet d'ajouter à la wishlist 
+//Sélectionnons tous les boutons wishlist de la page 
+const boutonsWishlist = document.querySelectorAll('.btn-wishlist'); 
+//Ensuite on ajoute un écouteur d'événement à chaque bouton
+boutonsWishlist.forEach(function(bouton){
+    bouton.addEventListener('click', function(){
+        //On va basculer la classe "active" pour changer l'apparence
+        bouton.classList.toggle('active'); 
+        //On va changer le SVG du bouton en fonction de son état 
+        if (bouton.classList.contains('active')){
+            //Si le coeur est plein 
+            bouton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/></svg>';
+        }else{
+            //Si le coeur est vide
+            bouton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/></svg>';
+        }
+    });
+});
